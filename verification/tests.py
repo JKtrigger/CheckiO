79a59ab872e86bea1dbae5cf4912df976697121a
JKtrigger <kasher89@mail.ru>
2014-12-21 02:13:06 +0300

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -10,4 +10,27 @@ TESTS = {
                     'yellow-cat','Winfield-Swede','tea-Marlboro'],['fish-colors']),
             "answer": "green"
         },
+        
+            
+        {
+            "input": (['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+                    'German-coffee','beer-white','cat-water',
+                    'horse-2','milk-3','4-Rothmans',
+                    'dog-Swede','Norwegian-1','horse-Marlboro',
+                    'bird-Brit','4-green','Winfield-beer',
+                    'Dane-blue','5-dog','blue-horse',
+                    'yellow-cat','Winfield-Swede','tea-Marlboro'],['Brit-cigarettes'])
+            "answer": 'Pall Mall'
+        },
+         {
+            "input": (['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+                    'German-coffee','beer-white','cat-water',
+                    'horse-2','milk-3','4-Rothmans',
+                    'dog-Swede','Norwegian-1','horse-Marlboro',
+                    'bird-Brit','4-green','Winfield-beer',
+                    'Dane-blue','5-dog','blue-horse',
+                    'yellow-cat','Winfield-Swede','tea-Marlboro'],['coffee-colors'])
+            "answer": 'green'
+        },
+        
     

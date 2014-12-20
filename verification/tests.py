d4fc2762e2a74a651665d2a479f62e14c9b09ce5
JKtrigger <kasher89@mail.ru>
2014-12-21 00:15:38 +0300

    Create tests.py

---
+++ verification/tests.py
@@ -0,0 +1,13 @@
+TESTS = {
+    "Basics": [
+        {
+            "input": (['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+                    'German-coffee','beer-white','cat-water',
+                    'horse-2','milk-3','4-Rothmans',
+                    'dog-Swede','Norwegian-1','horse-Marlboro',
+                    'bird-Brit','4-green','Winfield-beer',
+                    'Dane-blue','5-dog','blue-horse',
+                    'yellow-cat','Winfield-Swede','tea-Marlboro'],['fish-colors']),
+            "answer": "green"
+        },
+    

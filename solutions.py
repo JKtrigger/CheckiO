faa9dede836c99efcea49cc11255819a68edbb12
JKtrigger <kasher89@mail.ru>
2014-12-21 16:16:27 +0300

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -63,7 +63,7 @@ def checkio(data,question):
     
     #"Answer"
 if __name__ == '__main__':
-    '''assert checkio(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
+    assert checkio(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
                     'German-coffee','beer-white','cat-water',
                     'horse-2','milk-3','4-Rothmans',
                     'dog-Swede','Norwegian-1','horse-Marlboro',
@@ -83,14 +83,8 @@ if __name__ == '__main__':
                     'dog-Swede','Norwegian-1','horse-Marlboro',
                     'bird-Brit','4-green','Winfield-beer',
                     'Dane-blue','5-dog','blue-horse',
-                    'yellow-cat','Winfield-Swede','tea-Marlboro'],['Norwegian-potables'])=='water' #3st test'''
-    assert checkio(['Norwegian-Dunhill','Marlboro-blue','Brit-3',
-                    'German-coffee','beer-white','cat-water',
-                    'horse-2','milk-3','4-Rothmans',
-                    'dog-Swede','Norwegian-1','horse-Marlboro',
-                    'bird-Brit','4-green','Winfield-beer',
-                    'Dane-blue','5-dog','blue-horse',
-                    'yellow-cat','Winfield-Swede','tea-Marlboro'],['coffee-colors'])=='green'
+                    'yellow-cat','Winfield-Swede','tea-Marlboro'],['Norwegian-potables'])=='water' #3st test
+
     
     
                         
